bbb354d3bdfc249bd206fb9018e406260f558252
rafael <rafaelmchaves@gmail.com>
2017-11-04 15:54:57 +0000

    Creating spring boot project

---
+++ app/src/main/java/transfer/service/ingenico/IngenicoApplication.java
@@ -0,0 +1,12 @@
+package transfer.service.ingenico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngenicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngenicoApplication.class, args);
+	}
+}
